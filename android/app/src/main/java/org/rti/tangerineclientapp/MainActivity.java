cd70a85b2b389b3bf105125b3b8046cb9846461c
niteshmandall <108911562+niteshmandall@users.noreply.github.com>
2026-01-20 12:40:15 +0530

    feat: add deeplink support

--- android/app/src/main/java/org/rti/tangerineclientapp/MainActivity.java
+++ android/app/src/main/java/org/rti/tangerineclientapp/MainActivity.java
@@ -1,5 +1,32 @@
 package org.rti.tangerineclientapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+  private static final String TAG = "Main001";
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    handleDeepLink(getIntent());
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    handleDeepLink(intent);
+  }
+
+  private void handleDeepLink(Intent intent) {
+    String url = intent.getDataString();
+    if (url != null) {
+      Log.d(TAG, "handleDeepLink: " + url);
+    }
+  }
+}
