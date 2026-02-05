ccb935b7bd7d3d695e72b19c2d6a0eb9cee99176
faridy-mahvish <mahvish.faridy@amiti.in>
2026-02-05 14:00:15 +0530

    added back button to form list

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,21 +1,52 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import {IonHeader, IonToolbar, IonTitle, IonIcon, IonButton, IonButtons} from '@ionic/angular/standalone';
 import { LogoutButtonComponent } from '../logout-button/logout-button.component';
+import {Capacitor} from '@capacitor/core';
+import {NavigationEnd, Router} from '@angular/router';
+import { Location } from '@angular/common';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, LogoutButtonComponent]
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, LogoutButtonComponent, IonIcon, IonButton, IonButtons]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
   @Input() showLogout = true;
   @Output() logout = new EventEmitter<void>();
-  
+  showBackButtonOnNativeDevice = false;
+  constructor(private location: Location, private router: Router) { }
+  isNative = Capacitor.isNativePlatform();
+
+  ngOnInit() {
+    this.updateBackButton(this.isNative, this.router.url);
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateBackButton(this.isNative, event.urlAfterRedirects);
+      });
+  }
+
+  private updateBackButton(isNative: boolean, path: string) {
+    this.showBackButtonOnNativeDevice =
+      isNative && path.startsWith('/forms');
+  }
+
   onLogout(): void {
-	this.logout.emit();
+	  this.logout.emit();
+  }
+
+  onBackClick(): void {
+    if (window.history.length > 1) {
+      this.location.back();
+    } else {
+      // fallback route
+      this.router.navigateByUrl('/groups');
+    }
   }
 }
 
